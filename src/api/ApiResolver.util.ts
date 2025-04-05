89eed8c2ddedb1199580f04e9d1382be1360c23e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-04-06 01:08:49 +0300

    feat(api): created ApiResolver.util.ts

---
+++ src/api/ApiResolver.util.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import apiConf from "./api.conf.ts";
+
+interface RequestConfig<T> extends AxiosRequestConfig {
+  data?: T;
+}
+
+class ApiResolverUtil {
+  private readonly endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  async request<U, S>(
+    url: string,
+    method: string,
+    data?: U,
+    jwt?: string,
+  ): Promise<S> {
+    const fullUrl = `${apiConf.endpoint}/${this.endpoint}/${url}`;
+
+    const config: RequestConfig<U> = {
+      url: fullUrl,
+      method,
+      data,
+      headers: {
+        Authorization: jwt ? `Bearer ${jwt}` : undefined,
+      },
+    };
+
+    try {
+      const response: AxiosResponse<S> = await axios(config);
+      return response.data;
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        throw error;
+      } else {
+        throw new Error('Неизвестная ошибка');
+      }
+    }
+  }
+}
+
+export default ApiResolverUtil;
